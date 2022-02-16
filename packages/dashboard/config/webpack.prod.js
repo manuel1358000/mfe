7216a65b991bc60d2042636f52aa5c18bb3432b4
Manuel <manuel13580@gmail.com>
2022-02-15 21:35:24 -0600

    refactor(frontend): update deployment

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -1,6 +1,5 @@
 const {merge} = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-
 const commonConfig = require('../config/webpack.common');
 const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN;
@@ -8,15 +7,15 @@ const prodConfig={
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/container/latest/'
+        publicPath: '/dashboard/latest/'
     },
     plugins:[
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes:{
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-                auth: `auth@${domain}/auth/latest/remoteEntry.js`,
-                dashboard: `dashboard@${domain}/dashboard/latest/remoteEntry.js`
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes:{
+                './DashboardApp':'./src/bootstrap',
+                
             },
             shared: packageJson.dependencies
         })
